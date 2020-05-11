42e27a5690c965507b698e2cb08e83e7d0c8f545
Takayuki Maruyama <bis5.wsys@gmail.com>
2020-05-11 19:19:41 +0900

    primefaces-5884

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,6 +1,9 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -9,18 +12,40 @@ import javax.inject.Named;
 @ViewScoped
 public class TestView implements Serializable {
     
-    private String testString;
+    private List<TestRecord> records = new ArrayList<>();
     
     @PostConstruct  
     public void init() {
-        testString = "Welcome to PrimeFaces!!!";
+        records.add(new TestRecord("record1"));
+        records.add(new TestRecord("record2"));
+        records.add(new TestRecord("record3"));
     }
 
-    public String getTestString() {
-        return testString;
+    public List<TestRecord> getRecords() {
+        return records;
     }
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+    public void setRecords(List<TestRecord> records) {
+        this.records = records;
+    }
+
+    public static class TestRecord implements Serializable {
+        private String value;
+
+        public TestRecord() {
+            this(null);
+        }
+
+        public TestRecord(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
 }
