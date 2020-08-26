db5068750b0cf41990033e43da7f0d2aab2794fc
Takayuki Maruyama <bis5.wsys@gmail.com>
2020-08-26 15:44:22 +0900

    Test for MenuButton's SubMenu support

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -5,22 +5,37 @@ import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
+import org.primefaces.model.menu.Submenu;
+
 @Named
 @ViewScoped
 public class TestView implements Serializable {
+
+    private MenuModel menuModel = new DefaultMenuModel();
     
-    private String testString;
-    
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces!!!";
+    @PostConstruct
+    void initialize() {
+        menuModel.getElements().add(DefaultMenuItem.builder()
+            .value("Open")
+            .build());
+        Submenu subMenu = DefaultSubMenu.builder()
+            .icon("pi pi-briefcase")
+            .label("New")
+            .addElement(DefaultMenuItem.builder()
+                .value("Project")
+                .build())
+            .addElement(DefaultMenuItem.builder()
+                .value("Other")
+                .build())
+            .build();
+        menuModel.getElements().add(subMenu);
     }
 
-    public String getTestString() {
-        return testString;
+    public MenuModel getMenuModel() {
+        return menuModel;
     }
-
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
 }
