44b83c833467eca774274d994f3452d4ae2abb98
Takayuki Maruyama <bis5.wsys@gmail.com>
2020-10-04 22:29:43 +0900

    OmniFaces ToCollectionConverter bug reproducer
    ToCollectionConverter + any converter = ClassCastException

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,26 +1,46 @@
 package org.primefaces.test;
 
-import java.io.Serializable;
-import javax.annotation.PostConstruct;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-@Named
-@ViewScoped
-public class TestView implements Serializable {
+import javax.enterprise.inject.Model;
+
+@Model
+public class TestView {
+
+    public enum TestEnum {
+        FIRST, SECOND, THIRD;
+    }
     
-    private String testString;
-    
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces!!!";
+    private List<TestEnum> selection = new ArrayList<>();
+
+    public List<TestEnum> getSelection() {
+        return selection;
     }
 
-    public String getTestString() {
-        return testString;
+    public void setSelection(List<TestEnum> selection) {
+        this.selection = selection;
     }
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+    private Set<TestEnum> parameter = new HashSet<>();
+
+    public Set<TestEnum> getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(Set<TestEnum> parameter) {
+        this.parameter = parameter;
+    }
+
+    public List<TestEnum> getSelectableItems() {
+        return Arrays.asList(TestEnum.values());
+    }
+
+    public String submit() {
+        return "test.xhtml?faces-redirect=true&parameter=" + selection.stream().map(TestEnum::toString).collect(Collectors.joining(","));
+    }
 }
