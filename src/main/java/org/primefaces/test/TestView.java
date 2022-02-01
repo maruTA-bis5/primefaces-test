0d8027c1cbde8532bb5d970c6aeea01ae0a230d0
Takayuki Maruyama <bis5.wsys@gmail.com>
2022-02-01 22:30:30 +0000

    DataTable + addRow + multiple selection

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,27 +1,35 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.math.BigDecimal;
-import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import lombok.Data;
+import lombok.Value;
 
 @Data
 @Named
 @ViewScoped
 public class TestView implements Serializable {
     
-    private String string;
-    private Integer integer;
-    private BigDecimal decimal;
-    private LocalDateTime localDateTime;
-    
-    @PostConstruct  
-    public void init() {
-        string = "Welcome to PrimeFaces!!!";
+    private List<RowData> data = new ArrayList<>();
+
+    private List<RowData> selection = new ArrayList<>();
+
+    public void onAddRow() {
+        data.add(new RowData());
+    }
+
+    @Value
+    public static class RowData implements Serializable {
+        RowData() {
+            this.uuid = UUID.randomUUID().toString();
+        }
+        private String uuid;
     }
 
 }
