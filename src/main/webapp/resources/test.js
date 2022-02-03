03d9e1614959dcaa790bfdcfe502f461d2bf4dc5
Takayuki Maruyama <bis5.wsys@gmail.com>
2022-02-03 15:46:51 +0000

    Chart.js + beginAtZero

--- src/main/webapp/resources/test.js
+++ src/main/webapp/resources/test.js
@@ -1,1 +1,7 @@
 /* Place any Javascript here you want loaded in test.xhtml */
+window.chartExtender = function() {
+    // Rough Workaround
+    // this.cfg.config.options.scales.y.beginAtZero = this.cfg.config.options.scales.y.ticks.beginAtZero;
+    console.info(this.cfg.config);
+    debugger;
+};
